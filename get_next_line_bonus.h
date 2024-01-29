c4a604e745f74a2380ce8fdf620a9b6422154a28
Onur Gokkaya <ogokkaya@k2m03s05.42kocaeli.com.tr>
2024-01-29 19:08:23 +0300

    get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogokkaya <ogokkaya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 21:47:43 by ogokkaya          #+#    #+#             */
+/*   Updated: 2024/01/20 04:43:01 by ogokkaya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+void	*ft_free(char **str, int len);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(const char *s, const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
